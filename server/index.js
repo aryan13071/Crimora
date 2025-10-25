dfd6368825b6d75eed49807122e02fb4d2213b0a
aryan13071 <coderaryan2005@gmail.com>
2025-10-25 14:54:00 +0530

    upgrade

--- server/index.js
+++ server/index.js
@@ -71,7 +71,7 @@ const __dirname = path.dirname(__filename);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  app.get('/:path(*)', (req, res) => {
+    res.send('Not Found');
   });
 }
